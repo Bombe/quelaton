54d230d3c4a3deadd54c5fe63cd8c2d21d4d7b9d
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-09-06 21:37:10 +0200

    Add javadoc and reformat

--- src/test/java/net/pterodactylus/fcp/test/AbstractClientCommandTest.java
+++ src/test/java/net/pterodactylus/fcp/test/AbstractClientCommandTest.java
@@ -3,12 +3,13 @@ package net.pterodactylus.fcp.test;
 import org.junit.Rule;
 
 /**
- * TODO
+ * Abstract base class for all client command tests.
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
 public class AbstractClientCommandTest {
 
-	@Rule public final WithFcp fcp = new WithFcp();
+	@Rule
+	public final WithFcp fcp = new WithFcp();
 
 }
