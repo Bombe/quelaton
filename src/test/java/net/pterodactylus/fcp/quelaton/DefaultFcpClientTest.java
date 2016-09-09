c81bb1ca658a4837e0c27c6b90f659aae451b344
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-09-09 23:32:19 +0200

    Move tests for “ClientGet” client command to their own class

--- src/test/java/net/pterodactylus/fcp/quelaton/DefaultFcpClientTest.java
+++ src/test/java/net/pterodactylus/fcp/quelaton/DefaultFcpClientTest.java
@@ -26,12 +26,9 @@ import net.pterodactylus.fcp.ConfigData;
 import net.pterodactylus.fcp.FcpKeyPair;
 import net.pterodactylus.fcp.Key;
 import net.pterodactylus.fcp.NodeData;
-import net.pterodactylus.fcp.Priority;
 import net.pterodactylus.fcp.RequestProgress;
 import net.pterodactylus.fcp.fake.FakeTcpServer;
-import net.pterodactylus.fcp.quelaton.ClientGetCommand.Data;
 
-import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import com.nitorcreations.junit.runners.NestedRunner;
 import org.hamcrest.Description;
@@ -269,152 +266,6 @@ public class DefaultFcpClientTest {
 				"RequestURI=" + REQUEST_URI + "",
 				"Identifier=" + identifier,
 				"EndMessage");
-		}
-
-	}
-
-	public class ClientGet {
-
-		@Test
-		public void works() throws InterruptedException, ExecutionException, IOException {
-			Future<Optional<Data>> dataFuture = fcpClient.clientGet().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "ReturnType=direct"));
-			replyWithAllData("not-test", "Hello World", "text/plain;charset=latin-9");
-			replyWithAllData(identifier, "Hello", "text/plain;charset=utf-8");
-			Optional<Data> data = dataFuture.get();
-			verifyData(data);
-		}
-
-		@Test
-		public void getFailedIsRecognized() throws InterruptedException, ExecutionException, IOException {
-			Future<Optional<Data>> dataFuture = fcpClient.clientGet().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt"));
-			replyWithGetFailed("not-test");
-			replyWithGetFailed(identifier);
-			Optional<Data> data = dataFuture.get();
-			assertThat(data.isPresent(), is(false));
-		}
-
-		@Test
-		public void getFailedForDifferentIdentifierIsIgnored()
-		throws InterruptedException, ExecutionException, IOException {
-			Future<Optional<Data>> dataFuture = fcpClient.clientGet().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt"));
-			replyWithGetFailed("not-test");
-			replyWithAllData(identifier, "Hello", "text/plain;charset=utf-8");
-			Optional<Data> data = dataFuture.get();
-			verifyData(data);
-		}
-
-		@Test(expected = ExecutionException.class)
-		public void connectionClosedIsRecognized() throws InterruptedException, ExecutionException, IOException {
-			Future<Optional<Data>> dataFuture = fcpClient.clientGet().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt"));
-			fcpServer.close();
-			dataFuture.get();
-		}
-
-		@Test
-		public void withIgnoreData() throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().ignoreDataStore().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "IgnoreDS=true"));
-		}
-
-		@Test
-		public void withDataStoreOnly() throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().dataStoreOnly().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "DSonly=true"));
-		}
-
-		@Test
-		public void clientGetWithMaxSizeSettingSendsCorrectCommands()
-		throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().maxSize(1048576).uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "MaxSize=1048576"));
-		}
-
-		@Test
-		public void clientGetWithPrioritySettingSendsCorrectCommands()
-		throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().priority(Priority.interactive).uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "PriorityClass=1"));
-		}
-
-		@Test
-		public void clientGetWithRealTimeSettingSendsCorrectCommands()
-		throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().realTime().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "RealTimeFlag=true"));
-		}
-
-		@Test
-		public void clientGetWithGlobalSettingSendsCorrectCommands()
-		throws InterruptedException, ExecutionException, IOException {
-			fcpClient.clientGet().global().uri("KSK@foo.txt").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=KSK@foo.txt", "Global=true"));
-		}
-
-		@Test
-		public void clientGetFollowsRedirect() throws InterruptedException, ExecutionException, IOException {
-		    Future<Optional<Data>> data = fcpClient.clientGet().uri("USK@foo/bar").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=USK@foo/bar"));
-			replyWithRedirect("USK@foo/baz");
-			readMessage(() -> matchesFcpMessage("ClientGet", "URI=USK@foo/baz"));
-			replyWithAllData(identifier, "Hello", "text/plain;charset=utf-8");
-			verifyData(data.get());
-		}
-
-		@Test
-		public void clientGetNotifiesListenersOnRedirect() throws IOException, ExecutionException, InterruptedException {
-			List<String> redirects = new ArrayList<>();
-			Future<Optional<Data>> data = fcpClient.clientGet().onRedirect(redirects::add).uri("USK@foo/bar").execute();
-			connectAndAssert(() -> matchesFcpMessage("ClientGet", "URI=USK@foo/bar"));
-			replyWithRedirect("USK@foo/baz");
-			readMessage(() -> matchesFcpMessage("ClientGet", "URI=USK@foo/baz"));
-			replyWithRedirect("USK@foo/quux");
-			readMessage(() -> matchesFcpMessage("ClientGet", "URI=USK@foo/quux"));
-			replyWithAllData(identifier, "Hello", "text/plain;charset=utf-8");
-			verifyData(data.get());
-			assertThat(redirects, contains("USK@foo/baz", "USK@foo/quux"));
-		}
-
-		private void replyWithGetFailed(String identifier) throws IOException {
-			fcpServer.writeLine(
-				"GetFailed",
-				"Identifier=" + identifier,
-				"Code=3",
-				"EndMessage"
-			);
-		}
-
-		private void replyWithRedirect(String newUri) throws IOException {
-			fcpServer.writeLine(
-				"GetFailed",
-				"Identifier=" + identifier,
-				"Code=27",
-				"RedirectURI=" + newUri,
-				"EndMessage"
-			);
-		}
-
-		private void replyWithAllData(String identifier, String text, String contentType) throws IOException {
-			fcpServer.writeLine(
-				"AllData",
-				"Identifier=" + identifier,
-				"DataLength=" + (text.length() + 1),
-				"StartupTime=1435610539000",
-				"CompletionTime=1435610540000",
-				"Metadata.ContentType=" + contentType,
-				"Data",
-				text
-			);
-		}
-
-		private void verifyData(Optional<Data> data) throws IOException {
-			assertThat(data.get().getMimeType(), is("text/plain;charset=utf-8"));
-			assertThat(data.get().size(), is(6L));
-			assertThat(ByteStreams.toByteArray(data.get().getInputStream()),
-				is("Hello\n".getBytes(StandardCharsets.UTF_8)));
 		}
 
 	}
