a1a159a60e09fc4f35a08a1b8c5caa87e0a9e17e
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-04-15 12:48:10 +0200

    Replace AddPeer command with Kotlin version

---
+++ src/main/kotlin/net/pterodactylus/fcp/quelaton/AddPeerCommandImpl.kt
@@ -0,0 +1,64 @@
+package net.pterodactylus.fcp.quelaton
+
+import com.google.common.util.concurrent.*
+import net.pterodactylus.fcp.*
+import java.io.*
+import java.net.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.concurrent.atomic.*
+import java.util.function.*
+
+/**
+ * Default [AddPeerCommand] implementation based on [FcpDialog].
+ *
+ * @author [David ‘Bombe’ Roden](mailto:bombe@freenetproject.org)
+ */
+internal class AddPeerCommandImpl(threadPool: ExecutorService, private val connectionSupplier: ConnectionSupplier, private val identifierSupplier: Supplier<String>) : AddPeerCommand {
+
+	private val threadPool: ListeningExecutorService = MoreExecutors.listeningDecorator(threadPool)
+	private val file = AtomicReference<File>()
+	private val url = AtomicReference<URL>()
+	private val nodeRef = AtomicReference<NodeRef>()
+
+	override fun fromFile(file: File) =
+			this.file.set(file).let {
+				Executable { this.execute() }
+			}
+
+	override fun fromURL(url: URL) =
+			this.url.set(url).let {
+				Executable { this.execute() }
+			}
+
+	override fun fromNodeRef(nodeRef: NodeRef) =
+			this.nodeRef.set(nodeRef).let {
+				Executable { this.execute() }
+			}
+
+	private fun execute(): ListenableFuture<Optional<Peer>> {
+		return threadPool.submit<Optional<Peer>>(this::executeDialog)
+	}
+
+	private fun executeDialog() =
+			when {
+				file.get() != null -> AddPeer(identifierSupplier.get(), file.get().path)
+				url.get() != null -> AddPeer(identifierSupplier.get(), url.get())
+				else -> AddPeer(identifierSupplier.get(), nodeRef.get())
+			}.let { addPeer ->
+				AddPeerDialog().use { addPeerDialog ->
+					addPeerDialog.send(addPeer).get()!!
+				}
+			}
+
+	private inner class AddPeerDialog : FcpDialog<Optional<Peer>>(threadPool, connectionSupplier.get(), Optional.empty()) {
+
+		override fun consumePeer(peer: Peer) {
+			result = Optional.ofNullable(peer)
+		}
+
+		override fun consumeProtocolError(protocolError: ProtocolError) = finish()
+
+	}
+
+}
